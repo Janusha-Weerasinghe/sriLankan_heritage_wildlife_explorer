342404cf35f33be93b5543774646c963e6f6cac4
Janusha-Weerasinghe <amjanushachamali@gmail.com>
2025-04-22 08:40:19 +0530

    Modified folder structure

---
+++ srilankan_heritage_wildlife_explorer/srilankan_heritage_wildlife_explorer/UI/Common/Theme/Colors.swift
@@ -0,0 +1,7 @@
+//
+//  Colors.swift
+//  srilankan_heritage_wildlife_explorer
+//
+//  Created by Janusha 023 on 2025-04-22.
+//
+
---
+++ srilankan_heritage_wildlife_explorer/srilankan_heritage_wildlife_explorer/UI/Screens/Main/WildlifeExplorerScreen.swift
@@ -0,0 +1,7 @@
+//
+//  WildlifeExplorerScreen.swift
+//  srilankan_heritage_wildlife_explorer
+//
+//  Created by Janusha 023 on 2025-04-22.
+//
+
