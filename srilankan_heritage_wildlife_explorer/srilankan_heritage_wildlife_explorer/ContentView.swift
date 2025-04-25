116d7d9ab588272ca8dffe10d32c8c8068a8d925
Janusha Chamali Weerasinghe <140795815+Janusha-Weerasinghe@users.noreply.github.com>
2025-04-26 04:13:09 +0530

    Update ContentView.swift

--- srilankan_heritage_wildlife_explorer/srilankan_heritage_wildlife_explorer/ContentView.swift
+++ srilankan_heritage_wildlife_explorer/srilankan_heritage_wildlife_explorer/ContentView.swift
@@ -5,23 +5,23 @@
 //  Created by janusha on 2025-04-23.
 //
 
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    SplashView()
-}
+//import SwiftUI
+//
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
+//
+//#Preview {
+//    SplashView()
+//}
 //struct ContentView: View {
 //    var body: some View {
 //        MainView()
@@ -30,3 +30,18 @@ struct ContentView: View {
 //    }
 //}
 
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+            .environmentObject(AuthManager())
+            .environmentObject(LocationManager())
+    }
+}
+
+#Preview {
+    ContentView()
+        .environmentObject(AuthManager())
+        .environmentObject(LocationManager())
+}
